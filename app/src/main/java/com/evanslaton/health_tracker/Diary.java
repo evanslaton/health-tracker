0300b0932d72b593c697278d5ec11f14ee9176ce
Evan Slaton <evanslaton@gmail.com>
2019-01-16 10:57:37 -0800

    Can view exercise history

--- app/src/main/java/com/evanslaton/health_tracker/Diary.java
+++ app/src/main/java/com/evanslaton/health_tracker/Diary.java
@@ -3,14 +3,14 @@ package com.evanslaton.health_tracker;
 import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import java.util.List;
 
 public class Diary extends AppCompatActivity {
-
     protected ExerciseDatabase exerciseDatabase;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -29,35 +29,42 @@ public class Diary extends AppCompatActivity {
                 .fallbackToDestructiveMigration()
                 .build();
 
+        // Improves performance if changes in content do not change the layout size of the RecyclerView
+        // mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
         // Specify an adapter (see also next example)
-        mAdapter = new MyAdapter(myDataset);
+        mAdapter = new MyAdapter(exerciseDatabase.exerciseDao().getAll());
         mRecyclerView.setAdapter(mAdapter);
 
+        //
         if (exerciseDatabase.exerciseDao().getAll().isEmpty()) {
             Exercise newExercise = new Exercise("Bench Press", 10, "Push heavy things against gravity");
             exerciseDatabase.exerciseDao().insertExercise(newExercise);
         }
-
-
-
     }
 
-    public void showExercises() {
-        List<Exercise> exercises = exerciseDatabase.exerciseDao().getAll();
-
-    }
-
+//    public void showExercises() {
+//        List<Exercise> exercises = exerciseDatabase.exerciseDao().getAll();
+//    }
 
     public void addExercise(View v) {
-        TextView textViewTitle = findViewById(R.id.exerciseTitle);
-        TextView textViewReps = (findViewById(R.id.reps));
-        TextView textViewDescription = findViewById(R.id.description);
+        EditText editViewTitle = findViewById(R.id.exerciseTitle);
+        EditText editViewReps = (findViewById(R.id.reps));
+        EditText editViewDescription = findViewById(R.id.description);
 
-        String title = textViewTitle.getText().toString();
-        int reps = Integer.parseInt(textViewReps.getText().toString());
-        String description = textViewDescription.getText().toString();
+        String title = editViewTitle.getText().toString();
+        int reps = Integer.parseInt(editViewReps.getText().toString());
+        String description = editViewDescription.getText().toString();
 
         Exercise newExercise = new Exercise(title, reps, description);
         exerciseDatabase.exerciseDao().insertExercise(newExercise);
+
+        // https://stackoverflow.com/questions/3053761/reload-activity-in-android - from Jessica Lovell
+        finish();
+        startActivity(getIntent());
     }
 }
--- app/src/main/java/com/evanslaton/health_tracker/MyAdapter.java
+++ app/src/main/java/com/evanslaton/health_tracker/MyAdapter.java
@@ -2,53 +2,57 @@ package com.evanslaton.health_tracker;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private List<Exercise> mDataset;
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<Exercise> exercises;
 
     // Provides a reference to the views for each data item
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView mTextView;
-        public MyViewHolder(TextView v) {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public View mView;
+        public TextView title;
+        public TextView description;
+
+        public ViewHolder(View v) {
             super(v);
-            mTextView = v;
+            mView = v;
+            title = (TextView) v.findViewById(R.id.firstLine);
+            description = (TextView) v.findViewById(R.id.secondLine);
         }
     }
 
-    // Constructor
+    // MyAdapter constructor
     public MyAdapter(List<Exercise> exercises) {
-        mDataset = exercises;
+        this.exercises = exercises;
     }
 
     // Create new views (invoked by the layout manager)
     @Override
-    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
-                                                     int viewType) {
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.row_layout, parent, false);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.row_layout, parent, false);
 
-        MyViewHolder vh = new MyViewHolder(v);
+        // set the view's size, margins, padding and layout parameters
+        ViewHolder vh = new ViewHolder(v);
         return vh;
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.mTextView.setText(mDataset[position]);
-
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // Get elements from the dataset and replaces the contents of the view with the element
+        holder.title.setText(exercises.get(position).quantity + " " + exercises.get(position).title + " " + exercises.get(position).timestamp);
+        holder.description.setText(exercises.get(position).description);
     }
 
-    // Returns the number of exercise stored in the database
+    // Returns the size of the dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.size();
+        return exercises.size();
     }
 }
