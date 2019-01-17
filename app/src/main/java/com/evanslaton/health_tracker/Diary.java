c63d95cba7df3e0a1fd1646d4b1936a76a3e3d63
Evan Slaton <evanslaton@gmail.com>
2019-01-17 13:15:20 -0800

    Diary shows both exercise from the HerokuHerokuDB and the localDB

--- app/src/main/java/com/evanslaton/health_tracker/Diary.java
+++ app/src/main/java/com/evanslaton/health_tracker/Diary.java
@@ -5,8 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // From Google's Android docs and http://www.vogella.com/tutorials/AndroidRecyclerView/article.html
 public class Diary extends AppCompatActivity {
@@ -14,6 +27,7 @@ public class Diary extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private List<Exercise> exercises = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +42,15 @@ public class Diary extends AppCompatActivity {
                 .fallbackToDestructiveMigration()
                 .build();
 
+        exercises = exerciseDatabase.exerciseDao().getAll();
+        getExercisesFromHerokuDB();
+
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         // Specify an adapter (see also next example)
-        mAdapter = new MyAdapter(exerciseDatabase.exerciseDao().getAll());
+        mAdapter = new MyAdapter(exercises);
         mRecyclerView.setAdapter(mAdapter);
 
         // Adds an exercise to the database if the database is empty to avoid a NullPointerException
@@ -41,6 +58,43 @@ public class Diary extends AppCompatActivity {
             Exercise newExercise = new Exercise("Bench Press", 10, "Push heavy things against gravity");
             exerciseDatabase.exerciseDao().insertExercise(newExercise);
         }
+    }
+
+    // Gets exercises from health-tracker-backend database
+    public void getExercisesFromHerokuDB() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://health-tracker-backend.herokuapp.com/exercises";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest =
+                new StringRequest(
+                        Request.Method.GET,
+                        url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Gson gson = new Gson();
+                                Exercise[] exercisesFromHerokuDB = gson.fromJson(response, Exercise[].class);
+
+                                // Combine Heroku DB and local DB
+                                if (exercisesFromHerokuDB.length > 0) {
+                                    for (Exercise e : exercisesFromHerokuDB) {
+//                                        System.out.println("This is e: " + e);
+                                        exercises.add(e);
+                                    }
+                                }
+                                System.out.println(exercises);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // mTextView.setText("That didn't work!");
+                        Log.e("Error", "That didn't work!");
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
     // Saves user input to the database
